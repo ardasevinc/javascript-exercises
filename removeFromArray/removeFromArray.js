426aea065dc10a61731dbd2caf3d025b1d187100
ardasevinc <ardasevinc4@gmail.com>
2019-07-10 14:25:11 +0300

    finished

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,5 +1,21 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, args) {
+    if (arguments.length === 2) {
+        arr.splice(arr.indexOf(args), 1);
+        return arr;
+    }
+    else if (arguments.length > 2) {
+        let indexOfRemoved;
 
+        for (let i=1; i<arguments.length; i++) {
+            if (arr.indexOf(arguments[i]) === -1) continue;
+            else {
+                indexOfRemoved = arr.indexOf(arguments[i])
+                arr.splice(indexOfRemoved, 1);
+            }
+        }
+        return arr;
+    }
+    else return console.error("Illegal argument");
 }
 
 module.exports = removeFromArray
--- reverseString/reverseString.js
+++ reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    splitString = string.split("");
+    reversedString = "";
 
+    for (let i = splitString.length - 1; i >= 0; i--) {
+        reversedString += splitString[i];
+    }
+    return reversedString;
 }
 
 module.exports = reverseString
