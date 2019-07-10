7bae766d4c053cf9e492389298fe0e8dd2ad31d9
ardasevinc <ardasevinc4@gmail.com>
2019-07-10 14:55:42 +0300

    finished

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,5 +1,28 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    if ((typeof a === "number") && (typeof b === "number")) {
+        if (a < 0 || b < 0) return "ERROR";
+        else {
+            let numBetween = max(a, b) - min(a, b) + 1;
+            return (numBetween * (numBetween + 1)) / 2;
+        }
+    }
 
+    else return "ERROR";
+}
+
+function max(x, y) {
+    if (x > y) return x;
+    else if (y > x) return y;
+    else if (x == y) return x;
+    else return "COMPARISON ERROR";
+}
+
+
+function min(x, y) {
+    if (x > y) return y;
+    else if (y > x) return x;
+    else if (x == y) return x;
+    else return "COMPARISON ERROR";
 }
 
 module.exports = sumAll
