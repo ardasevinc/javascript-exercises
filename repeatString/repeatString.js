c4159e7e186ee74b8b1bc8818286d31f965c9ece
ardasevinc <ardasevinc4@gmail.com>
2019-07-10 12:58:52 +0300

    finished

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function(string, n) {
+    let smallString = string;
+    
+    if (n === 0) return "";
+    else if (n === 1) return string;
+    else if (n > 1) {
+        for(let i=0; i<n-1; i++) {
+            string += smallString;
+        }
+        return string;
+    }
+    else return "ERROR";
 }
 
 module.exports = repeatString
